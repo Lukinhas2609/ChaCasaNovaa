24dc95bb591c855c868ef78592be87f169608386
Lukas Damasceno <lukas.a@uni9.edu.br>
2026-02-18 18:51:43 -0300

    Corrigindo caminho do script

--- public/presentes.js
+++ public/presentes.js
@@ -1,4 +1,3 @@
-// ===== SUPABASE =====
 const supabaseUrl = "https://tkakohssjokjfzdwhbmc.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrYWtvaHNzam9ramZ6ZHdoYm1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNTA0NDMsImV4cCI6MjA4NjgyNjQ0M30.YOY1rmVWZwlAvRB3vTcxjoPpGO6iHEO66CYWlh1CnpU";
 
